b099b2596a1aceb8078ae095196943fc8a521233
Giuseppe <piscoroma@gmail.com>
2026-05-03 13:48:30 +0200

    inventory/feat: ensure streams exist for order.consumer

--- services/inventory-service/src/messaging/order.consumer.js
+++ services/inventory-service/src/messaging/order.consumer.js
@@ -3,7 +3,8 @@ const { runWithContext, getContext } = require('../observability/context_storage
 const { headers } = require('nats');
 const { AlreadyProcessingError } = require('../errors/inventory.errors');
 
-const STREAM_NAME = 'ORDERS';
+const ORDERS_STREAM = 'ORDERS'; 
+const INVENTORY_STREAM = 'INVENTORY';
 const CONSUMER_NAME = 'inventory-service';
 
 const MAX_RETRIES = 5;
@@ -27,13 +28,36 @@ function createOrderConsumer({ natsClient, inventoryService, logger }) {
       }
       const jsm = await nc.jetstreamManager();
 
+      // check that stream to consume exists
       try {
-         await jsm.consumers.info(STREAM_NAME, CONSUMER_NAME);
+         await jsm.streams.info(ORDERS_STREAM);
+         logger.info('Stream found,', { stream: ORDERS_STREAM });
+      } catch (_) {
+         throw new Error(`Stream ${ORDERS_STREAM} does not exist`);
+      }
+
+      // check that stream to publish exists. Create it if not exists
+      try {
+         await jsm.streams.info(INVENTORY_STREAM);
+         logger.info('Stream found', { stream: INVENTORY_STREAM });
+      } catch (_) {
+         await jsm.streams.add({
+            name: INVENTORY_STREAM,
+            subjects: ['inventory.>'],
+            storage: 'memory',
+            retention: 'limits',
+         });
+         logger.info('Stream created', { stream: INVENTORY_STREAM });
+      }
+      
+      // consume on ORDERS stream
+      try {
+         await jsm.consumers.info(ORDERS_STREAM, CONSUMER_NAME);
          logger.info('Consumer already exists', { consumer: CONSUMER_NAME });
       } catch (_) {
-         await jsm.consumers.add(STREAM_NAME, {
+         await jsm.consumers.add(ORDERS_STREAM, {
             durable_name: CONSUMER_NAME,
-            filter_subject: 'order.created',
+            filter_subject: 'order.*',
             ack_policy: 'explicit',
             deliver_policy: 'all',
             ack_wait: 30 * 1e9,
@@ -42,8 +66,7 @@ function createOrderConsumer({ natsClient, inventoryService, logger }) {
          logger.info('Consumer created', { consumer: CONSUMER_NAME });
       }
 
-      const consumer = await js.consumers.get(STREAM_NAME, CONSUMER_NAME);
-
+      const consumer = await js.consumers.get(ORDERS_STREAM, CONSUMER_NAME);
       await startConsuming(consumer);
    }
 
