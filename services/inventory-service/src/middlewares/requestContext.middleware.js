6d40f13c4aec6f88206ada2b76e73b56fa2549ee
Giuseppe <piscoroma@gmail.com>
2026-05-18 22:59:21 +0200

    inventory/fix: correct factory of requestContext.middleware.js

--- services/inventory-service/src/middlewares/requestContext.middleware.js
+++ services/inventory-service/src/middlewares/requestContext.middleware.js
@@ -1,15 +1,13 @@
 const { runWithContext } = require('../observability/context_storage');
 const { randomUUID } = require('crypto');
 
-function requestContextMw() {
-   return (req, res, next) => {
-      const correlationId = req.headers['x-correlation-id'] || randomUUID();
+function requestContextMw(req, res, next) {
+   const correlationId = req.headers['x-correlation-id'] || randomUUID();
 
-      runWithContext({ correlationId }, () => {
-         res.setHeader('x-correlation-id', correlationId);
-         next();
-      });
-   };
+   runWithContext({ correlationId }, () => {
+      res.setHeader('x-correlation-id', correlationId);
+      next();
+   });
 }
 
 module.exports = requestContextMw;
