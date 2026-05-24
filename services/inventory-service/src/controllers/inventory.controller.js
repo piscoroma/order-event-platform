468e9c8c02d538513b6931349d8508b4eaf2cd7f
Giuseppe <piscoroma@gmail.com>
2026-05-24 21:22:27 +0200

    inventory/feat: check that body is not null on requests

--- services/inventory-service/src/controllers/inventory.controller.js
+++ services/inventory-service/src/controllers/inventory.controller.js
@@ -1,34 +1,24 @@
+const { ValidationError } = require("@order-event-platform/shared/errors/base.errors");
+
 function createInventoryController({ inventoryService }) {
 
-   async function listItems(req, res, next) {
-      try {
-         const items = await inventoryService.listItems();
-         res.json(items);
-      } catch (err) {
-         next(err);
-      }
+   async function listItems(req, res) {
+      const items = await inventoryService.listItems();
+      res.json(items);
    }
 
-   async function getItem(req, res, next) {
-      try {
-         const item = await inventoryService.getItem(req.params.id);
-         res.json(item);
-      } catch (err) {
-         next(err);
-      }
+   async function getItem(req, res) {
+      const item = await inventoryService.getItem(req.params.id);
+      res.json(item);
    }
 
-   async function updateItem(req, res, next) {
-      try {
-         const { stock } = req.body;
-         if (typeof stock !== 'number' || stock < 0) {
-            return res.status(400).json({ error: 'stock must be a non-negative number' });
-         }
-         const item = await inventoryService.updateItemStock(req.params.id, stock);
-         res.json(item);
-      } catch (err) {
-         next(err);
-      }
+   async function updateItem(req, res) {
+      const { stock } = req.body ?? {};
+      if (!stock || typeof stock !== 'number' || stock < 0)
+         throw new ValidationError(`stock must be a non-negative number`);
+
+      const item = await inventoryService.updateItemStock(req.params.id, stock);
+      res.json(item);
    }
 
    return { 
