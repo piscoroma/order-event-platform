885af7af9f0628b8536f66c19d086969d2268906
Giuseppe <piscoroma@gmail.com>
2026-09-10 17:48:05 +0200

    shared: add more buckets on `http_request_duration_seconds` metric

--- shared/observability/http.metrics.js
+++ shared/observability/http.metrics.js
@@ -10,11 +10,21 @@ function createHttpMetrics({ register }) {
    });
 
    const httpRequestDuration = new client.Histogram({
-   name: 'http_request_duration_seconds',
-   help: 'Duration of HTTP requests in seconds',
-   labelNames: ['method', 'route', 'status'],
-   buckets: [0.01, 0.05, 0.1, 0.5, 1, 2],
-   registers: [register],
+      name: 'http_request_duration_seconds',
+      help: 'Duration of HTTP requests in seconds',
+      labelNames: ['method', 'route', 'status'],
+      buckets: [
+         0.01,   // 10 ms
+         0.025,  // 25 ms
+         0.05,   // 50 ms
+         0.1,    // 100 ms
+         0.2,    // 200 ms
+         0.25,   // 250 ms
+         0.5,    // 500 ms
+         1,      // 1 s
+         2       // 2 s
+      ],
+      registers: [register],
    });
 
    return {
